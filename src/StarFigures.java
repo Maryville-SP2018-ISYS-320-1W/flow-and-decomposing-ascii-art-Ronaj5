0b2c31595de3a9fbb0b10b3119f293c1c9af5baf
Ronaj5 <37316864+Ronaj5@users.noreply.github.com>
2018-03-18 16:13:57 -0500

    Tricky

--- src/StarFigures.java
+++ src/StarFigures.java
@@ -1,16 +1,54 @@
 /*
   	ISYS 320
-  	Name(s):
-  	Date: 
+  	Name(s): Rona Davis
+  	Date: March 17, 2018
 */
 
 // 4. Your pseudocode algorithm for how to break down the figure
 
 
 public class StarFigures {
+			public static void main (String [] args)  {
+				Draw2Horiz();
+				drawX();
+				System.out.println();
+				System.out.println();
+				Draw2Horiz();
+				drawX();
+				Draw2Horiz();
+				System.out.println();
+				System.out.println();
+				drawVert();
+				drawX();
+				Draw2Horiz();
+			}
 
-	public static void main(String[] args) {
+			private static void Draw2Horiz() {
+				// TODO Auto-generated method stub
+				System.out.println("*********");
+				System.out.println("*********");
+			}
 
-	}
+			private static void drawVert() {
+				// TODO Auto-generated method stub
+				System.out.println("    *");
+				System.out.println("    *");
+				System.out.println("    *");
+				
+			}
 
-}
+			private static void drawX() {
+				// TODO Auto-generated method stub
+				System.out.println("\\      /");
+				System.out.println(" \\    /");
+				System.out.println("  \\  /");
+				System.out.println("   \\/");
+				System.out.println("   /\\");
+				System.out.println("  /  \\");
+				System.out.println(" /    \\");
+			}
+
+		}
+	
+
+
--- src/Tricky.java
+++ src/Tricky.java
@@ -5,13 +5,34 @@
 */
 
 // 1. Your predicted output 
- 
+//Done with main
+//This is message1.
+//This is message2. This is message1.
+//Done with message 2.
+ // 3. Were you correct? Explain any differences  2 differences:  
+//I started with the first System.out.println and 
+//for message 2 I kept message1 on same line since no println
 
 public class Tricky {
+	    public static void main(String[] args) {
+	        message1();
+	        message2();
+	        System.out.println("Done with main.");
+	    }
+	    
+	    public static void message1() {
+	        System.out.println("This is message1.");
+	    }
+	    
+	    public static void message2() {
+	        System.out.println("This is message2.");
+	        message1();
+	        System.out.println("Done with message 2.");
+	    }
+	}
 
-}
 
-// 3. Were you correct? Explain any differences
+
  
  
 
