c0943e3ca40e8664d116e49a14fb70a17356bed4
Ronaj5 <37316864+Ronaj5@users.noreply.github.com>
2018-03-18 18:10:30 -0500

    RepeatedMessages

---
+++ src/RepeatedMessages.java
@@ -0,0 +1,8 @@
+
+public class RepeatedMessages {
+		public static void main(String[] args) {
+			for (int i=0; i< 1000; i++) {
+			   System.out.println("Milhouse did not test cootie positive!");
+		}
+	}
+}
--- src/maryville/edu/RepeatedMessages.java
+++ src/maryville/edu/RepeatedMessages.java
@@ -1,5 +1,10 @@
 package maryville.edu;
 
 public class RepeatedMessages {
-
+	public static void main(String[] args) {
+		for (int i=1; i<= 1000; i++) {
+			System.out.println(i + " ");
+			
+			}
+		}
 }
